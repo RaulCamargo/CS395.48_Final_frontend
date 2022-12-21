4cee30933942e7f30105f3e13eda5a71a85c9b4d
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-21 15:28:54 -0500

    + employee & tassk reducer. updated reducer index

--- assignment-tracker/src/store/reducers/index.js
+++ assignment-tracker/src/store/reducers/index.js
@@ -2,3 +2,5 @@
 // The aliases of reducers in this file will be assigned as the names of the keys in the Redux store, with the values being the respective individual reducers;
 export { default as allEmployees } from './employees';
 export { default as allTasks } from './tasks';
+export { default as employee } from './employee';
+export { default as task } from './task';
