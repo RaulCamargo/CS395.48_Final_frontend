65799481da6174fdb3dd454bb0bcd84b0b6be881
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-21 15:39:18 -0500

    updated container index

--- assignment-tracker/src/components/containers/index.js
+++ assignment-tracker/src/components/containers/index.js
@@ -2,4 +2,5 @@
 export { default as HomePageContainer } from "./HomePageContainer";
 export { default as AllEmployeesContainer } from "./AllEmployeesContainer";
 export { default as AllTasksContainer } from "./AllTasksContainer";
-
+export { default as EmployeeContainer } from "./EmployeeContainer";
+export { default as TaskContainer } from "./TaskContainer";
