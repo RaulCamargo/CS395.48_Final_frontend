518f55c4536bff93346088e87f0a8985363e6f63
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-21 06:52:06 -0500

    created all task, all employee, home views & index

---
+++ assignment-tracker/src/components/views/index.js
@@ -0,0 +1,4 @@
+// Barrel file for presentational components, which will be used to compose our smart containers;
+export { default as AllEmployeesView } from "./AllEmployeesView";
+export { default as AllTasksView } from "./AllTasksView";
+export { default as HomePageView } from "./HomePageView";
