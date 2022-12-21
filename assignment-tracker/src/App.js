fb78a917ea3b32df5b8883240d6f4c90af834495
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-21 18:21:38 -0500

    updated app.js

--- assignment-tracker/src/App.js
+++ assignment-tracker/src/App.js
@@ -9,6 +9,7 @@ import {
   AllTasksContainer,
   EmployeeContainer,
   TaskContainer,
+  EditTaskContainer
 } from './components/containers';
 
 const App = () => {
@@ -18,8 +19,10 @@ const App = () => {
         <Route exact path="/" element={<HomePageContainer />} />
         <Route exact path="/employees" element={<AllEmployeesContainer />} />
         <Route exact path="/tasks" element={<AllTasksContainer />} />
-        <Route exact path="/employee/:id" component={<EmployeeContainer />} />
-        <Route exact path="/task/:id" component={<TaskContainer />} />
+        <Route exact path="/employee/:id" element={<EmployeeContainer />} />
+        <Route exact path="/task/:id" element={<TaskContainer />} />
+        <Route exact path="/edittask/:id" element={<EditTaskContainer />} />
+
       </Routes>        
     </div>
   );
