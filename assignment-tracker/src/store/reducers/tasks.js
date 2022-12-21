208d91c08ef8451f3ae87e23bb3d0515bdc42bda
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-21 16:53:34 -0500

    added edit employee/task reducers

--- assignment-tracker/src/store/reducers/tasks.js
+++ assignment-tracker/src/store/reducers/tasks.js
@@ -5,6 +5,12 @@ const allTasks = (state=[], action) => {
   switch (action.type) {
     case at.FETCH_ALL_TASKS:
       return action.payload;
+    case at.EDIT_TASK:
+      return state.map(task => { 
+        return (
+          task.id===action.payload.id ? action.payload : task
+        );
+      });
     
     default:
       return state;
