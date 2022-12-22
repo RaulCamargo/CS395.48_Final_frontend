0e6a0203f27f4eae1f7549bb2cd1999c2aa440b0
Raul <Raul.Camargo89@myHunter.CUNY.edu>
2022-12-21 20:09:14 -0500

    updated container index

--- assignment-tracker/src/components/containers/index.js
+++ assignment-tracker/src/components/containers/index.js
@@ -5,3 +5,4 @@ export { default as AllTasksContainer } from "./AllTasksContainer";
 export { default as EmployeeContainer } from "./EmployeeContainer";
 export { default as TaskContainer } from "./TaskContainer";
 export { default as EditTaskContainer } from "./EditTaskContainer";
+export { default as EditEmployeeContainer} from "./EditEmployeeContainer";
